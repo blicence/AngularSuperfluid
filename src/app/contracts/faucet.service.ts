a8e85deaf533a26b25ad72ecef07dc333be440c5
zinderud <mokosam@gmail.com>
2022-11-18 01:27:38 +0300

    foucet

--- src/app/contracts/faucet.service.ts
+++ src/app/contracts/faucet.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ethers } from 'ethers';
+import { constants, ethers, utils } from 'ethers';
 import Faucet from '../artifacts/Faucet.json'
-
+import SuperToken from '../artifacts/SuperToken.json'
 import { environment } from '../../environments/environment';
 import { DefaultProviderService } from '../shared/providers/default-provider.service';
 import { WalletProviderService } from '../shared/providers/wallet-provider.service';
@@ -12,12 +12,18 @@ import { WalletProviderService } from '../shared/providers/wallet-provider.servi
 })
 export class FaucetService {
 
-    faucetContract: any
+    faucetContract: any;
+    SuperContract: any;
 
     constructor(provider: DefaultProviderService, private wallet: WalletProviderService, private http: HttpClient) {
         this.faucetContract = new ethers.Contract(
             wallet.currentConfig.superToken.fUSDC,
             Faucet.abi,
+            provider.provider
+        );
+        this.SuperContract = new ethers.Contract(
+            wallet.currentConfig.superToken.fUSDCx,
+            SuperToken.abi,
             provider.provider
         );
     }
@@ -27,6 +33,19 @@ export class FaucetService {
         let amountSuper = 1000 * 10 ** 18;
         await this.faucetContract.connect(this.wallet.signer)['mint(uint256)'](amount);
     }
+    async approve() {
+        await this.faucetContract.connect(this.wallet.signer).approve(
+            environment.superToken.fUSDCx,
+            constants.MaxUint256
+        )
+    }
+    async upgrade() {
+        const value = utils.parseEther('1000000').toString();
+
+        await this.SuperContract.connect(this.wallet.signer).upgrade(value);
+
+    }
+
 
 
 
